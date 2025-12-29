f458d29d97f9084f33dfc730b903388c8d36732f
Folder <1275217027@qq.com>
2025-12-29 15:24:15 +0800

    12.29 15：24

--- VulExplainer/dpcnn/DPCNN.py
+++ VulExplainer/dpcnn/DPCNN.py
@@ -39,18 +39,15 @@ class DPCNN(nn.Module):
         self.fc = nn.Linear(dim_channel, num_class)
 
     def forward(self, input_ids):
-        # 获取词嵌入
-        emb_x = self.encoder.embeddings(input_ids)  # [batch_size, seq_len, emb_dim]
 
-        # 注意力掩码
+        x = self.encoder.embeddings(input_ids)  # [batch_size, seq_len, emb_dim]
         attention_mask = (
-            input_ids.ne(self.tokenizer.pad_token_id).unsqueeze(-1).expand_as(emb_x)
+            input_ids.ne(self.tokenizer.pad_token_id).unsqueeze(-1).expand_as(x)
         )
-
-        emb_x = emb_x * attention_mask  # [batch_size, seq_len, emb_dim]
+        x = x * attention_mask  # [batch_size, seq_len, emb_dim]
 
         # 调整维度用于卷积 [batch_size, 1, seq_len, emb_dim]
-        x = emb_x.unsqueeze(1)
+        x = x.unsqueeze(1)
 
         # 初始卷积
         x = self.initial_conv(x)  # [batch_size, dim_channel, seq_len, 1]
@@ -69,63 +66,4 @@ class DPCNN(nn.Module):
 
         # 输出层
         x = self.dropout(x)
-        return x
-
-
-class DPCNN2(nn.Module):
-
-    def __init__(
-        self, encoder, tokenizer, dim_channel, num_blocks, num_class, args
-    ):
-        super(DPCNN2, self).__init__()
-        self.encoder = encoder
-        self.tokenizer = tokenizer
-        self.args = args
-        emb_dim = encoder.config.hidden_size
-        self.conv_region = nn.Conv2d(1, dim_channel, (3, emb_dim), stride=1)
-        self.conv = nn.Conv2d(dim_channel, dim_channel, (3, 1), stride=1)
-        self.max_pool = nn.MaxPool2d(kernel_size=(3, 1), stride=2)
-        self.padding1 = nn.ZeroPad2d((0, 0, 1, 1))  # top bottom
-        self.padding2 = nn.ZeroPad2d((0, 0, 0, 1))  # bottom
-        self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(0.1)
-        self.bn = nn.BatchNorm2d(num_features=dim_channel)
-        self.fc = nn.Linear(dim_channel, num_class)
-
-    def forward(self, input_ids):
-        x = self.encoder.embeddings(input_ids)
-        attention_mask = (
-            input_ids.ne(self.tokenizer.pad_token_id).unsqueeze(-1).expand_as(x)
-        )
-        x = x * attention_mask  # [batch_size, seq_len, emb_dim]
-        x = x.unsqueeze(1)  # [batch_size, dim_channel, seq_len, 1]
-        x = self.conv_region(x)  # [batch_size, dim_channel, seq_len-3+1, 1]
-
-        x = self.padding1(x)  # [batch_size, dim_channel, seq_len, 1]
-        x = self.relu(x)
-        x = self.conv(x)  # [batch_size, dim_channel, seq_len-3+1, 1]
-        x = self.padding1(x)  # [batch_size, dim_channel, seq_len, 1]
-        x = self.relu(x)
-        x = self.conv(x)  # [batch_size, dim_channel, seq_len-3+1, 1]
-        while x.size()[2] > 2:
-            x = self._block(x)
-        x = x.squeeze()  # [batch_size, dim_channel]
-        x= self.dropout(x)
-        # x = self.fc(x)
-        return x
-
-    def _block(self, x):
-        x = self.padding2(x)
-        px = self.max_pool(x)
-
-        x = self.padding1(px)
-        x = F.relu(x)
-        x = self.conv(x)
-
-        x = self.padding1(x)
-        x = F.relu(x)
-        x = self.conv(x)
-
-        # Short Cut
-        x = x + px
         return x
--- VulExplainer/dpcnn/model.py
+++ VulExplainer/dpcnn/model.py
@@ -3,29 +3,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 import scipy.sparse as sp
 from utils import preprocess_adj, preprocess_features  # 假设utils中包含这些函数
-from DPCNN import DPCNN,DPCNN2
-from ReGCN import ReGCN
-from TextRCNN import TextRCNN
+from DPCNN import DPCNN
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# class PredictionClassification(nn.Module):
-#     """Head for sentence-level classification tasks."""
 
-#     def __init__(self, args, input_size,num_classes):
-#         super().__init__()
-#         if input_size is None:
-#             input_size = args.hidden_size
-#         self.dense = nn.Linear(input_size, args.hidden_size)
-#         self.dropout = nn.Dropout(0.1)
-#         self.out_proj = nn.Linear(args.hidden_size, num_classes)
-
-#     def forward(self, features):  #
-#         x = features
-#         x = self.dropout(x)
-#         x = self.dense(x.float())
-#         x = torch.tanh(x)
-#         x = self.dropout(x)
-#         x = self.out_proj(x)
-#         return x
 
 
 class FusionModel(nn.Module):
@@ -37,7 +17,6 @@ class FusionModel(nn.Module):
         self.args = args
 
         self.dpcnn_out_dim = args.hidden_size  # DPCNN的通道维度
-        self.regcn_out_dim = args.hidden_size 
         self.num_class = num_class
         # 初始化DPCNN组件
         self.dpcnn = DPCNN(
@@ -48,56 +27,14 @@ class FusionModel(nn.Module):
             num_class=num_class,
             args=self.args,
         )
-        self.TextRCNN = TextRCNN(
-            encoder=self.encoder,
-            tokenizer=self.tokenizer,
-            num_class=num_class,
-            args=self.args,
-        )
-        self.w_embeddings = self.encoder.embeddings.word_embeddings.weight.data.cpu().detach().clone().numpy()
-
-        self.regcn = ReGCN(
-            feature_dim_size=encoder.config.hidden_size,
-            hidden_size=self.regcn_out_dim,
-            dropout=0.1,
-        )
-        # 特征融合分类头
-        # self.fusion_dim = self.dpcnn_out_dim + self.regcn_out_dim
-        self.fusion_dim = self.dpcnn_out_dim
-
-        # self.classifier = PredictionClassification(
-        #     args, input_size=self.fusion_dim, num_classes=self.num_class
-        # )
-        self.classifier = nn.Linear(self.fusion_dim, self.num_class)
+        self.classifier = nn.Linear(self.dpcnn_out_dim, self.num_class)
 
     def forward(self, input_ids,labels):
         # 获取DPCNN的特征（使用return_hidden_state参数获取池化后的特征）
         dpcnn_features = self.dpcnn(input_ids)  # [batch_size, dim_channel]
-        # rcnn_features = self.TextRCNN(input_ids)
-        # outputs = self.encoder(
-        #     input_ids, attention_mask=input_ids.ne(self.tokenizer.pad_token_id)
-        # ).last_hidden_state
-        # outputs = outputs[:, 0, :]
-        
-        # # 获取ReGCN的特征（通过修改GNNReGVD的forward方法支持返回特征）
-        # adj, x_feature = build_graph(input_ids.cpu().detach().numpy(), self.w_embeddings, window_size=3)
-        # adj, adj_mask = preprocess_adj(adj)
-        # adj_feature = preprocess_features(x_feature)
-        # adj = torch.from_numpy(adj)
-        # adj_mask = torch.from_numpy(adj_mask)
-        # adj_feature = torch.from_numpy(adj_feature)
-        # regcn_features = self.regcn(
-        #     adj_feature.to(device).double(),
-        #     adj.to(device).double(),
-        #     adj_mask.to(device).double(),
-        # )  # [batch_size, regcn_out_dim]
 
-        # fused_features = torch.cat(
-        #     [dpcnn_features, regcn_features], dim=1
-        # ).float()  # [batch_size, fusion_dim]
-        fused_features = dpcnn_features
         # 分类预测
-        logits = self.classifier(fused_features)  # [batch_size, num_class]
+        logits = self.classifier(dpcnn_features)  # [batch_size, num_class]
 
         if labels is not None:
             loss_fct = nn.CrossEntropyLoss()
@@ -105,78 +42,3 @@ class FusionModel(nn.Module):
             return loss
         else:
             return F.softmax(logits, dim=-1)
-
-weighted_graph = False
-print("using default unweighted graph")
-# build graph function
-def build_graph(shuffle_doc_words_list, word_embeddings, window_size=3):
-    # print('using window size = ', window_size)
-    x_adj = []
-    x_feature = []
-    y = []
-    doc_len_list = []
-    vocab_set = set()
-
-    for i in range(len(shuffle_doc_words_list)):
-        doc_words = shuffle_doc_words_list[i]
-        doc_len = len(doc_words)
-
-        doc_vocab = list(set(doc_words))
-        doc_nodes = len(doc_vocab)
-
-        doc_len_list.append(doc_nodes)
-        vocab_set.update(doc_vocab)
-
-        doc_word_id_map = {}
-        for j in range(doc_nodes):
-            doc_word_id_map[doc_vocab[j]] = j
-
-        # sliding windows
-        windows = []
-        if doc_len <= window_size:
-            windows.append(doc_words)
-        else:
-            for j in range(doc_len - window_size + 1):
-                window = doc_words[j : j + window_size]
-                windows.append(window)
-
-        word_pair_count = {}
-        for window in windows:
-            for p in range(1, len(window)):
-                for q in range(0, p):
-                    word_p_id = window[p]
-                    word_q_id = window[q]
-                    if word_p_id == word_q_id:
-                        continue
-                    word_pair_key = (word_p_id, word_q_id)
-                    # word co-occurrences as weights
-                    if word_pair_key in word_pair_count:
-                        word_pair_count[word_pair_key] += 1.0
-                    else:
-                        word_pair_count[word_pair_key] = 1.0
-                    # bi-direction
-                    word_pair_key = (word_q_id, word_p_id)
-                    if word_pair_key in word_pair_count:
-                        word_pair_count[word_pair_key] += 1.0
-                    else:
-                        word_pair_count[word_pair_key] = 1.0
-
-        row = []
-        col = []
-        weight = []
-        features = []
-
-        for key in word_pair_count:
-            p = key[0]
-            q = key[1]
-            row.append(doc_word_id_map[p])
-            col.append(doc_word_id_map[q])
-            weight.append(word_pair_count[key] if weighted_graph else 1.0)
-        adj = sp.csr_matrix((weight, (row, col)), shape=(doc_nodes, doc_nodes))
-        x_adj.append(adj)
-
-        for k, v in sorted(doc_word_id_map.items(), key=lambda x: x[1]):
-            features.append(word_embeddings[k])
-        x_feature.append(features)
-
-    return x_adj, x_feature
